b109b5213c2e967786d93299422631b01f31a1a0
madistwn <madis.apsalon@twn.ee>
2023-10-07 13:55:42 +0300

    First commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+// Import the required modules using ES6 import syntax
+import express from 'express';
+
+// Create an instance of the Express application
+const app = express();
+
+// Define a route for the endpoint
+app.get('/hello', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Define the port on which the server will listen
+const port = 3000;
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
