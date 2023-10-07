00aad03b35929b52d110361fbedbcb6f48545c05
madistwn <madis.apsalon@twn.ee>
2023-10-07 22:54:23 +0300

    Modify port

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ app.get('/hello', (req, res) => {
 });
 
 // Define the port on which the server will listen
-const port = 3000;
+const port = 5000;
 
 // Start the server
 app.listen(port, () => {
